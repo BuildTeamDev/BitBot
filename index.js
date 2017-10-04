d10bb6ded8ec335ecae5be9da92d0f16c1418a2f
Marvin Hofmann <marvin@marv-productions.de>
2017-10-04 21:43:29 +0200

    maybe now

--- index.js
+++ index.js
@@ -68,7 +68,9 @@ function checkCommands(event) {
             event.message.channel.sendMessage("Error applying filter for command " +
                 command.name + " on event: ' " +
                 JSON.stringify(event) + "' is: \n " +
-                JSON.stringify(stackTrace.parse(err)));
+                stackTrace.parse(err).map(function (trace) {
+                    return '' + trace;
+                }));
         }
 
     }).forEach(function (command) {
@@ -78,7 +80,9 @@ function checkCommands(event) {
             event.message.channel.sendMessage("Error applying command " +
                 command.name + " on event: ' " +
                 JSON.stringify(event) + "' is: \n " +
-                JSON.stringify(stackTrace.parse(err)));
+                stackTrace.parse(err).map(function (trace) {
+                    return '' + trace;
+                }));
         }
     });
 }
