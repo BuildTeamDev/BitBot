6b489d225568559e8669ff8628b0aca993435177
Marvin Hofmann <marvin@marv-productions.de>
2017-10-04 21:34:42 +0200

    nodejs compatibility

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cluster = require('cluster');
 const http = require('http');
 const numCPUs = require('os').cpus().length;
-const stackinfo = require('stackinfo')
+const stackTrace = require('stack-trace');
 
 const PRICE_COMMAND = {
     check: function (event) {
@@ -68,7 +68,7 @@ function checkCommands(event) {
             event.message.channel.sendMessage("Error applying filter for command " +
                 command.name + " on event: ' " +
                 JSON.stringify(event) + "' is: \n " +
-                stackinfo(err));
+                stackTrace.parse((err));
         }
 
     }).forEach(function (command) {
@@ -78,7 +78,7 @@ function checkCommands(event) {
             event.message.channel.sendMessage("Error applying command " +
                 command.name + " on event: ' " +
                 JSON.stringify(event) + "' is: \n " +
-                stackinfo(err));
+                stackTrace.parse((err));
         }
     });
 }
