18685b7ab33e5821f7d97ab75a6200e6342aac0c
Marvin Hofmann <marvin@marv-productions.de>
2017-10-04 21:31:25 +0200

    better error stacktraces

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cluster = require('cluster');
 const http = require('http');
 const numCPUs = require('os').cpus().length;
+const stackinfo = require('stackinfo')
 
 const PRICE_COMMAND = {
     check: function (event) {
@@ -67,7 +68,7 @@ function checkCommands(event) {
             event.message.channel.sendMessage("Error applying filter for command " +
                 command.name + " on event: ' " +
                 JSON.stringify(event) + "' is: \n " +
-                JSON.stringify(err));
+                stackinfo(err));
         }
 
     }).forEach(function (command) {
@@ -77,7 +78,7 @@ function checkCommands(event) {
             event.message.channel.sendMessage("Error applying command " +
                 command.name + " on event: ' " +
                 JSON.stringify(event) + "' is: \n " +
-                JSON.stringify(err));
+                stackinfo(err));
         }
     });
 }
